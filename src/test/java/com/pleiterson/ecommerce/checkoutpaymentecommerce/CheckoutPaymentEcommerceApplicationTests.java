52f9b4365aecec32f0942bbf5fc1042ebe3b9e7f
pleiterson <pleiterson@gmail.com>
2021-03-09 19:06:45 -0300

    first commit

---
+++ src/test/java/com/pleiterson/ecommerce/checkoutpaymentecommerce/CheckoutPaymentEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pleiterson.ecommerce.checkoutpaymentecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutPaymentEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
